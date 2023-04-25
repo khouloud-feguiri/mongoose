02526830e95ce891c9d583f89455d51e1a17b79c
khouloud-feguiri <119670389+khouloud-feguiri@users.noreply.github.com>
2023-04-25 05:18:22 +0200

    Update Person.js

--- model/Person.js
+++ model/Person.js
@@ -1,1 +1,22 @@
 
+//Require mongoose
+const mongoose = require('mongoose');
+
+//Create schema
+const Schema = mongoose.Schema ;
+
+//Create person 
+const personSchema = new Schema ({
+    name : {
+        type : String ,
+        required : true 
+    },
+    age : Number ,
+
+    favoriteFoods : {
+      type :  [String]
+    },
+})
+
+//export 
+module.exports = Person = mongoose.model('person', personSchema)
